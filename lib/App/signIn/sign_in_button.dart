1a8cfeb6d77b4edd3e7cb09812cc5e0bb3be0513
YonasW <yonweldi@gmail.com>
2021-03-23 00:54:14 +0000

    Updated version

---
+++ lib/App/signIn/sign_in_button.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hudaccommodation_app/common_widgets/custom_raised_button.dart';
+
+class SignInButton extends CustomRaisedButton {
+  SignInButton({
+    String text,
+    Color color,
+    Color textColor,
+    VoidCallback onPressed,
+  }) : super(
+          child: Text(
+            text,
+            style: TextStyle(color: textColor, fontSize: 15.0),
+          ),
+          color: color,
+          height: 40.0,
+          onPressed: onPressed,
+        );
+}
